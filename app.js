2db7ddb0191820e3ee0e0860f4c9347a3d37dbe2
songxiaoliang <563609104@qq.com>
2017-12-02 14:04:12 +0800

    统一入口文件

--- app.js
+++ app.js
@@ -1,58 +1,76 @@
 /**
- * Created by shixia on 2017/7/12.
+ * react-native-share/login
+ * update by Songlcy 2017-12-02
  */
-
 import React, { Component } from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    NativeModules,
-    ToastAndroid
+  Text,
+  View,
+  NativeModules,
+  StyleSheet,
+  AppRegistry
 } from 'react-native';
 
+import SharePlatform from './SharePlatform'
+
 export default class ReactNativeShare extends Component {
+    
     /**
-     * content内容
-     * imgurl图片链接
-     * 平台：
-     *  0:QQ; 1:新浪; 2:微信; 3:微信朋友圈;4:FACEBOOK;
+     * 第三方分享
+     * 参数：标题、分享内容、分享链接、图片、平台、分享结果回调
      */
     _share() {
-        NativeModules.sharemodule.share('标题','内容', 'http://baidu.com','http://dev.umeng.com/images/tab2_1.png', 2,
-            (code, message) => {
-                ToastAndroid.show(message,ToastAndroid.SHORT);
-            });
+        NativeModules.sharemodule.share('标题','内容', 'http://baidu.com','http://dev.umeng.com/images/tab2_1.png', SharePlatform.QQ,
+        (message) => {
+        // message: 分享成功、分享失败、取消分享
+        // TODO ... 
+        });
     }
+
+    /**
+     * 第三方登录
+     * 参数：登录平台、登录结果回调
+     */
+    _getUserInfo() {
+        NativeModules.sharemodule.authLogin(SharePlatform.QQ, (result) => {
+            // code: 0成功、1失败、2取消
+            if(result.code === 0) {
+                console.log('授权登录成功:' + JSON.parse(result));
+            } else {
+                // TODO ...
+            }
+        });   
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome} onPress={this._share.bind(this)}>
-                    分享
+            <View style={ styles.container }>
+                <Text style={ styles.title } onPress={ this._share.bind(this) }>
+                    第三方分享
+                </Text>
+                <Text style={ styles.title } onPress={ this._getUserInfo.bind(this) }>
+                    第三方登录
                 </Text>
             </View>
         );
     }
 }
 
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+  
+    title: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    }
+  
 });
-
+  
 AppRegistry.registerComponent('ReactNativeShare', () => ReactNativeShare);
--- index.ios.js
+++ index.ios.js
@@ -2,76 +2,11 @@
  * react-native-share/login
  * update by Songlcy 2017-12-02
  */
-import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  NativeModules,
-  StyleSheet,
-  AppRegistry
-} from 'react-native';
+import App from './app';
 
-import SharePlatform from './SharePlatform'
 
-export default class ReactNativeShare extends Component {
 
-  /**
-   * 第三方分享
-   * 参数：标题、分享内容、分享链接、图片、平台、分享结果回调
-   */
-  _share() {
-     NativeModules.sharemodule.share('标题','内容', 'http://baidu.com','http://dev.umeng.com/images/tab2_1.png', SharePlatform.QQ,
-      (message) => {
-        // message: 分享成功、分享失败、取消分享
-        // TODO ... 
-    });
-  }
 
-  /**
-   * 第三方登录
-   * 参数：登录平台、登录结果回调
-   */
-  _getUserInfo() {
-    NativeModules.sharemodule.authLogin(SharePlatform.QQ, (result) => {
-      // code: 0成功、1失败、2取消
-      if(result.code === 0) {
-        console.log('授权登录成功:' + JSON.parse(result));
-      } else {
-        // TODO ...
-      }
-    });   
-  }
 
-  render() {
-    return (
-      <View style={ styles.container }>
-         <Text style={ styles.title } onPress={ this._share.bind(this) }>
-            第三方分享
-         </Text>
-         <Text style={ styles.title } onPress={ this._getUserInfo.bind(this) }>
-            第三方登录
-         </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-
-});
-
-AppRegistry.registerComponent('ReactNativeShare', () => ReactNativeShare);
 
 
